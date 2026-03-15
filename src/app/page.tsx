a9e8bf01c21c57357046fb263fb30e698343f717
ujjwalpandeyjava <ujjwalpandey.aps@gmail.com>
2026-03-15 20:27:27 +0530

    Added created by whom?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@mantine/core';
+import { Text, Button } from '@mantine/core';
 import Link from 'next/link';
 import { FaCloudUploadAlt } from 'react-icons/fa';
 import AudioPlayer from './AudioPlayer';
@@ -6,13 +6,16 @@ import AudioPlayer from './AudioPlayer';
 
 
 export default function Home() {
+  const isDev = process.env.NODE_ENV === 'development';
+
   return (
     <div className="home-page" >
       <AudioPlayer />
-      <Link href="/upload" className="uploadBtn">
+      <Text className="byUjjwal"><b>Created by:</b> Ujjwal Pandey</Text>
+      {isDev && <Link href="/upload" className="uploadBtn">
         <FaCloudUploadAlt size={20} />
         <span>Upload More</span>
-      </Link>
+      </Link>}
     </div>
   );
 }
