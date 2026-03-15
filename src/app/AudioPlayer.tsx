f3dbae118d409a57987a63949a7b48a7af406396
ujjwalpandeyjava <ujjwalpandey.aps@gmail.com>
2026-03-15 21:52:53 +0530

    Added - Auto play next song.

--- src/app/AudioPlayer.tsx
+++ src/app/AudioPlayer.tsx
@@ -218,7 +218,7 @@ const AudioPlayer = () => {
 								</Button>
 							</Group>
 
-							<audio ref={audioRef} onTimeUpdate={handleTimeUpdate} onLoadedMetadata={handleLoadedMetadata} />
+							<audio ref={audioRef} onTimeUpdate={handleTimeUpdate} onLoadedMetadata={handleLoadedMetadata} onEnded={handleNextTrack} />
 						</Flex>
 					</CardSection>
 				</Card>
